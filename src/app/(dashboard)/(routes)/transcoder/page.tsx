5d0b77c5f6a69bbf4455aa45a482523000bf1436
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-12-29 01:31:50 +0530

    uploadthing setup

--- src/app/(dashboard)/(routes)/transcoder/page.tsx
+++ src/app/(dashboard)/(routes)/transcoder/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Sidebar from "@/components/sidebar";
+import { UploadButton } from "@/utils/uploadthing";
 
 const page = () => {
   const sidebarItems = [{ name: "Video Transcoder", href: "/transcoder" }];
@@ -10,6 +12,20 @@ const page = () => {
       <div className="w-full">
         <div className="p-6 md:ml-0 ml-10">
           <h1 className="text-2xl font-bold">Transcoder</h1>
+        </div>
+        <div className="flex flex-col items-center justify-between p-24">
+          <UploadButton
+            endpoint="mediaPost"
+            onClientUploadComplete={(res) => {
+              // Do something with the response
+              console.log("Files: ", res);
+              alert("Upload Completed");
+            }}
+            onUploadError={(error: Error) => {
+              // Do something with the error.
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
         </div>
       </div>
     </div>
